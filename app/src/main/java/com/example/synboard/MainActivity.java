a915346904e936dc78e04ffa6a4226555f58dd6b
Ruby Duong <Yingjie@Rubys-MacBook-Air.local>
2019-07-14 20:06:23 -0500

    circle keys, switch between different layouts, start on the number layout

--- app/src/main/java/com/example/synboard/MainActivity.java
+++ app/src/main/java/com/example/synboard/MainActivity.java
@@ -2,12 +2,46 @@ package com.example.synboard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
+    Button enter;
+    TextView red, score, usersAnswer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        enter = findViewById(R.id.enter);
+        red = findViewById(R.id.fox);
+        score = findViewById(R.id.missed);
+        usersAnswer = findViewById(R.id.usersAnswer);
+
+        final String theFox[] = ("The quick red fox jumped over the lazy brown dog.").split("");
+
+        enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user[] = (usersAnswer.getText().toString()).split("");
+
+                int misses = 0;
+
+                System.out.println(theFox + ": " +  theFox.length);
+                System.out.println(user + " : " + user.length);
+                for(int i = 0; i < theFox.length && i < user.length; i++) {
+                    if(!theFox[i].toLowerCase().equals(user[i].toLowerCase())) {
+                        System.out.println(theFox[i].toLowerCase() + " : " + user[i].toLowerCase());
+                        misses++;
+                    }
+                }
+
+                score.setText("You have missed " + Integer.toString(misses) + " characters while typing with this layout");
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/synboard/MyInputMethodService.java
+++ app/src/main/java/com/example/synboard/MyInputMethodService.java
@@ -1,25 +1,33 @@
 package com.example.synboard;
 
+import android.content.Context;
 import android.inputmethodservice.*;
 import android.inputmethodservice.Keyboard;
 import android.inputmethodservice.KeyboardView;
+import android.os.Build;
+import android.os.VibrationEffect;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.inputmethod.InputConnection;
+import android.os.Vibrator;
 
 public class MyInputMethodService extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
     private KeyboardView kv;
-    private Keyboard keyboard;
+    private Keyboard keyboard, keyboard2, numbers_layout;
 
     private boolean caps = false;
+    boolean two = false;
+
 
     @Override
     public View onCreateInputView() {
         kv = (KeyboardView) getLayoutInflater().inflate(R.layout.keyboard_layout, null);
         keyboard = new Keyboard(this, R.xml.keys_layout);
+        keyboard2 = new Keyboard(this, R.xml.keys_layout2);
+        numbers_layout = new Keyboard(this, R.xml.keys_layout_numbers);
         kv.setKeyboard(keyboard);
         kv.setOnKeyboardActionListener(this);
         return kv;
@@ -42,6 +50,8 @@ public class MyInputMethodService extends InputMethodService implements Keyboard
     @Override
     public void onKey(int i, int[] ints) {
         InputConnection inputConnection = getCurrentInputConnection();
+        Vibrator vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        vibe.vibrate(10);
         if(inputConnection != null) {
             switch(i) {
                 case Keyboard.KEYCODE_DELETE:
@@ -53,6 +63,21 @@ public class MyInputMethodService extends InputMethodService implements Keyboard
                     else {
                         inputConnection.commitText("",1);
                     }
+
+                    if(two) {
+//                        keyboard = new Keyboard(this, R.xml.keys_layout);
+                        System.out.println("I am here: ");
+                        kv.setKeyboard(numbers_layout);
+                        two = false;
+                    }
+                    else {
+//                        keyboard = new Keyboard(this, R.xml.keys_layout2);
+                        System.out.println("I am here too: ");
+                        kv.setKeyboard(keyboard2);
+                        two = true;
+                    }
+
+                    kv.setOnKeyboardActionListener(this);
                 case Keyboard.KEYCODE_SHIFT:
                     caps = !caps;
                     keyboard.setShifted(caps);
